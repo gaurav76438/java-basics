00ef7904af7539f7e9a5aa9e47b08bb675786137
Gaurav Verma <aryagaurav774@gmail.com>
2025-11-20 22:13:31 +0530

    Add files via upload

---
+++ JAVA/static_java.java
@@ -0,0 +1,21 @@
+class Geeks{
+    static int a=40;
+    int b = 50;
+    void simpleDisplay(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+    static void staticDisplay(){
+        System.out.println(a);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Geeks obj= new Geeks();
+        obj.simpleDisplay();
+        
+        Geeks.staticDisplay();
+        // staticDisplay();
+    }
+}
